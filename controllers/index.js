208ac7f8fa371b6b165d343a08f5a5604209ef19
JAVI <ortiz.javier10@gmail.com>
2020-09-06 11:56:15 -0400

    final commit

--- controllers/index.js
+++ controllers/index.js
@@ -9,8 +9,5 @@ router.use('/', homeRoutes);
 
 router.use('/dashboard', dashboardRoutes);
 
-// router.use((req, res) => {
-//   res.status(404).end();
-// });
 
 module.exports = router;
--- models/Vote.js
+++ models/Vote.js
@@ -12,7 +12,7 @@ Vote.init(
       },
       user_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: {
           model: 'user',
           key: 'id'
